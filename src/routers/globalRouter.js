8c92c9b4df9834cfa88b7888a5132d65ec3e2c26
hmbgaq1 <hmbgaq1@gmail.com>
2022-07-02 16:03:21 +0900

    until architecture recap

---
+++ src/routers/globalRouter.js
@@ -0,0 +1,14 @@
+import express from "express";
+import { join } from "../controller/userController";
+import { trending } from "../controller/videoController";
+
+const globalRouter = express.Router();
+
+globalRouter.get("/", trending);
+globalRouter.get("/join", join);
+
+
+
+export default globalRouter;
+
+// module.exports = globalRouter;
---
+++ src/routers/videoRouter.js
@@ -0,0 +1,11 @@
+import express from "express";
+import { watch, edit } from "../controller/videoController";
+
+const videoRouter = express.Router();
+
+videoRouter.get("/watch", watch);
+videoRouter.get("/edit", edit);
+
+export default videoRouter;
+
+// module.exports = videoRouter;
