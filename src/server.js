ea022f31c82f3b621280747210cac5f8bd7c3a6a
hmbgaq1 <hmbgaq1@gmail.com>
2022-07-05 21:18:58 +0900

    environment variables

--- src/server.js
+++ src/server.js
@@ -1,6 +1,3 @@
-// import "./db";
-// import "./models/Video";
-
 import express from "express";
 import morgan from "morgan";
 import session from "express-session";
@@ -20,10 +17,13 @@ app.use(express.urlencoded({extended: true}))
 
 app.use(
     session({
-        secret: "Hello!",
+        secret: process.env.COOKIE_SECRET,
         resave: false,
         saveUninitialized: false,
-        store: MongoStore.create({ mongoUrl: "mongodb://127.0.0.1:27017/wetube" })
+        cookie: {
+            maxAge: 20000,
+        },
+        store: MongoStore.create({ mongoUrl: process.env.DB_URL }),
     })
 );
 
