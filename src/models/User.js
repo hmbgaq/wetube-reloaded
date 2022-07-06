80f528726e83dd8309371cf65939482ce7bcae69
hmbgaq1 <hmbgaq1@gmail.com>
2022-07-06 22:51:55 +0900

    user profile

--- src/models/User.js
+++ src/models/User.js
@@ -7,12 +7,17 @@ const userSchema = new mongoose.Schema({
   socialOnly: { type: Boolean, default: false },
   username: { type: String, required: true, unique: true },
   password: { type: String },
-  name: { type: String, required: true },
+  name: { type: String, require: true },
   location: String,
+  videos: [
+    { type: mongoose.Schema.Types.ObjectId, ref: "Video" }
+  ]
 });
 
 userSchema.pre('save', async function(){
-  this.password = await bcrypt.hash(this.password, 5);
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 5);
+  }
 })
 
 const User = mongoose.model("User", userSchema);
